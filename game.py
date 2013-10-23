c005330e4160d59a1c2a656e295fab53c4a883ec
graczyk <nick.graczyk@gmail.com>
2013-10-23 17:44:47 -0400

    final update to the files?

--- game.py
+++ game.py
@@ -1,22 +1,27 @@
-import os
-import psutil
+import os, subprocess
 
-PROCNAME = "Preview"
+print('Welcome to postscript tetris.')
 
-print('Welcome to postscript tetris. Depends on Preview.app on OS X for now')
-
-os.system('open -a Preview test.ps')
+p = subprocess.Popen("gv -watch -spartan test.ps", shell=True)
+if p < 0:
+    print('things went wrong')
 
 print('we will now test updating the PS file in the background')
 raw_input("Press Enter to continue...")
 
-str = open('test.ps','r').read()
-str += "72 600 moveto (Hell0 w0rld!) show showpage"
+str = '%! 
+ newpath 
+ 1.5 setlinewidth 
+ 65 65 moveto 
+ 65 0 rlineto
+ 0 65 rlineto 
+ -65 0 rlineto 
+ closepath
+ stroke
+ showpage'
 open('test.ps','w').write(str)
 
-#from http://stackoverflow.com/questions/2940858/kill-process-by-name-in-python
-for proc in psutil.process_iter():
-    if proc.name == PROCNAME:
-        proc.kill()
-#this doesn't solve the problem of having to close that window first because of shitty application behavior on OS X
-os.system('open -g -a Preview test.ps')
+
+raw_input("Press Enter to continue...")
+
+p.terminate()
